c3842886a29d70565e250d1ed3d325d22e2a3472
Jenecil Patac <jenecilpatac@gmail.com>
2024-11-25 10:38:14 +0800

    modified

---
+++ server/database/seeders/AdminSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = [
+            ['firstName' => 'Accounting Clerk', 
+            'lastName' => 'Accounting Clerk', 
+            'contact' => '09123456789',
+            'branch_code' => 'HO',
+            'branch' => 'HO',
+            'userName' => 'Admin',
+            'employee_id' => '123456',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456',
+            'position' => 'Admin',
+            'signature' => 'none',
+            'role' => 'Admin',
+            'created_at' => now(), 
+            'updated_at' => now()],
+      
+        ];
+
+        DB::table('users')->insert($admin);
+    }
+}
